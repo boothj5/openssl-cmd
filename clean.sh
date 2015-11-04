1627910788258cb6f50db603e33d66f758bd5c2d
James Booth <boothj5@gmail.com>
2015-11-04 21:11:27 +0000

    Added second example encrypting private keys

--- clean.sh
+++ clean.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 rm -f alice/alice_priv_key.pem
+rm -f alice/alice_priv_enc_key.pem
 rm -f alice/alice_pub_key.pem
 rm -f alice/bob_pub_key.pem
 
+rm -f bob/bob_priv_enc_key.pem
 rm -f bob/bob_priv_key.pem
 rm -f bob/bob_pub_key.pem
 rm -f bob/alice_pub_key.pem
