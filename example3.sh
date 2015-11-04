59e83db73b6f08232292228227fad69d08f99781
James Booth <boothj5@gmail.com>
2015-11-04 23:10:59 +0000

    Add comments

--- example3.sh
+++ example3.sh
@@ -5,14 +5,17 @@
 # Use des3 symmetric session key
 # Alice sends message to Bob
 
+# key generation
 echo
 echo "--> Generating Alice's private key"
 echo
 openssl genrsa -out alice/alice_priv_key.pem 4096
+
 echo
 echo "--> Extracting Alice's public key"
 echo
 openssl rsa -pubout -in alice/alice_priv_key.pem -out alice/alice_pub_key.pem
+
 echo
 echo "--> Encrypting Alice's private key"
 echo
@@ -22,40 +25,54 @@ echo
 echo "--> Generating Bob's private key"
 echo
 openssl genrsa -out bob/bob_priv_key.pem 4096
+
 echo
 echo "--> Extracting Bob's public key"
 echo
 openssl rsa -pubout -in bob/bob_priv_key.pem -out bob/bob_pub_key.pem
+
 echo
 echo "--> Encrypting Bobs's private key"
 echo
 openssl rsa -in bob/bob_priv_key.pem -des3 -out bob/bob_priv_enc_key.pem
 
+# key exchange
 echo "--> Exchaning public keys"
 cp alice/alice_pub_key.pem bob/.
 cp bob/bob_pub_key.pem alice/.
 
+# Alice sends
 echo "--> Alice creating session key"
 openssl rand 8 -hex > alice/session_key
+
 echo "--> Alice encrypting session key"
 openssl rsautl -encrypt -inkey alice/bob_pub_key.pem -pubin -in alice/session_key -out alice/session_key_ciphertext
+
 echo "--> Alice base64 encoding session key ciphertext"
 base64 alice/session_key_ciphertext > alice/session_key_ciphertext.base64
+
 echo "--> Alice encrypting plaintext with session key"
 openssl enc -des3 -in alice/plaintext -out alice/ciphertext -pass file:alice/session_key
+
 echo "--> Alice base64 encoding ciphertext"
 base64 alice/ciphertext > alice/ciphertext.base64
+
 echo "--> Alice sending message"
 cp alice/ciphertext.base64 bob/.
+
 echo "--> Alice sending session key"
 cp alice/session_key_ciphertext.base64 bob/.
 
+# Bob receives
 echo "--> Bob base64 decoding ciphertext"
 base64 -d bob/ciphertext.base64 > bob/ciphertext
+
 echo "--> Bob base64 decoding session key ciphertext"
 base64 -d bob/session_key_ciphertext.base64 > bob/session_key_ciphertext
+
 echo "--> Bob decrypting session_key_ciphertext"
 openssl rsautl -decrypt -inkey bob/bob_priv_enc_key.pem -in bob/session_key_ciphertext -out bob/session_key
+
 echo "--> Bob decrypting ciphertext"
 openssl enc -des3 -d -in bob/ciphertext -out bob/plaintext -pass file:bob/session_key
 
