895cc335d8e028dbf1073b634774e72cc4599e56
James Booth <boothj5@gmail.com>
2015-11-05 10:43:28 +0000

    Update script 1 with key presses

--- 1-simple.sh
+++ 1-simple.sh
@@ -7,7 +7,7 @@ error_handler()
 {
     ERR_CODE=$?
     tput setaf 1
-    echo "Error $ERR_CODE with command '$BASH_COMMAND' on line ${BASH_LINENO[0]}. Exiting."
+        echo "Error $ERR_CODE with command '$BASH_COMMAND' on line ${BASH_LINENO[0]}. Exiting."
     tput sgr0
     exit $ERR_CODE
 }
@@ -16,60 +16,53 @@ trap error_handler ERR
 
 # key generation
 tput setaf 2
-echo "--> Generating Alice's private key"
+    echo "--> Generate Alice's private key" ; read -n1 -s
 tput sgr0
 openssl genrsa -out alice/alice_priv_key.pem 4096
+cat alice/alice_priv_key.pem
 
 tput setaf 2
-echo "--> Extracting Alice's public key"
+    echo "--> Extract Alice's public key" ; read -n1 -s
 tput sgr0
 openssl rsa -pubout -in alice/alice_priv_key.pem -out alice/alice_pub_key.pem
+cat alice/alice_pub_key.pem
 
 tput setaf 2
-echo "--> Generating Bob's private key"
+    echo "--> Generate Bob's private key" ; read -n1 -s
 tput sgr0
 openssl genrsa -out bob/bob_priv_key.pem 4096
+cat bob/bob_priv_key.pem
 
 tput setaf 2
-echo "--> Extracting Bob's public key"
+    echo "--> Extract Bob's public key" ; read -n1 -s
 tput sgr0
 openssl rsa -pubout -in bob/bob_priv_key.pem -out bob/bob_pub_key.pem
+cat bob/bob_pub_key.pem
 
 # key exchange
 tput setaf 2
-echo "--> Exchaning public keys"
+    echo "--> Exchange public keys" ; read -n1 -s
 tput sgr0
 cp alice/alice_pub_key.pem bob/.
 cp bob/bob_pub_key.pem alice/.
 
 # Alice sends
 tput setaf 2
-echo "--> Alice encrypting plaintext"
+    echo "--> Alice encrypt plaintext" ; read -n1 -s
 tput sgr0
 openssl rsautl -encrypt -inkey alice/bob_pub_key.pem -pubin -in alice/plaintext -out alice/ciphertext
-
-tput setaf 2
-echo "--> Alice base64 encoding ciphertext"
-tput sgr0
 base64 alice/ciphertext > alice/ciphertext.base64
+cat alice/ciphertext.base64
 
 tput setaf 2
-echo "--> Alice sending message"
+    echo "--> Alice send message" ; read -n1 -s
 tput sgr0
 cp alice/ciphertext.base64 bob/.
 
 # Bob receives
 tput setaf 2
-echo "--> Bob base64 decoding ciphertext"
+    echo "--> Bob decrypt ciphertext" ; read -n1 -s
 tput sgr0
 base64 --decode bob/ciphertext.base64 > bob/ciphertext
-
-tput setaf 2
-echo "--> Bob decrypting ciphertext"
-tput sgr0
 openssl rsautl -decrypt -inkey bob/bob_priv_key.pem -in bob/ciphertext -out bob/plaintext
-
-tput setaf 2
-echo "--> Bob message received:"
-tput sgr0
 cat bob/plaintext
