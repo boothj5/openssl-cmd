14feff90b4319cbe96b848dee010f7bfa1b3cf5f
James Booth <boothj5@gmail.com>
2015-11-07 21:35:56 +0000

    Use colour functions in example 3

--- 3-session-key.sh
+++ 3-session-key.sh
@@ -8,67 +8,67 @@ PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 trap error_handler ERR
 trap exit_handler EXIT
 
-echo_wait $YELLOW "--> Bob: Generate PRIVATE KEY"
+echo_bob "Bob: Generate PRIVATE KEY"
 openssl genrsa -out bob/bob_priv_key.pem 4096
-cat bob/bob_priv_key.pem
+cat_unsafe bob/bob_priv_key.pem
 
-echo_wait $YELLOW "--> Bob: Encrypt PRIVATE_KEY"
+echo_bob "Bob: Encrypt PRIVATE_KEY"
 openssl rsa -in bob/bob_priv_key.pem -des3 -out bob/bob_priv_enc_key.pem
-cat bob/bob_priv_enc_key.pem
+cat_safe bob/bob_priv_enc_key.pem
 
-echo_wait $YELLOW "--> Bob: Extract PUBLIC KEY from PRIVATE KEY"
+echo_bob "Bob: Extract PUBLIC KEY from PRIVATE KEY"
 openssl rsa -pubout -in bob/bob_priv_enc_key.pem -out bob/bob_pub_key.pem
-cat bob/bob_pub_key.pem
+cat_safe bob/bob_pub_key.pem
 
-echo_wait $YELLOW "--> Bob: Send PUBLIC KEY to Alice"
+echo_bob "Bob: Send PUBLIC KEY to Alice"
 echo "cp bob/bob_pub_key.pem alice/."
 cp bob/bob_pub_key.pem alice/.
 
-echo_wait $CYAN "--> Alice: Create message"
+echo_alice "Alice: Create message"
 echo "Hello this is a private message from Alice to Bob..." > alice/plaintext
-cat alice/plaintext
+cat_unsafe alice/plaintext
 
-echo_wait $CYAN "--> Alice: Create SESSION KEY"
+echo_alice "Alice: Create SESSION KEY"
 openssl rand 8 -hex > alice/session_key
-cat alice/session_key
+cat_unsafe alice/session_key
 
-echo_wait $CYAN "--> Alice: Encrypt plaintext with SESSION KEY"
+echo_alice "Alice: Encrypt plaintext with SESSION KEY"
 openssl enc -des3 -in alice/plaintext -out alice/ciphertext -pass file:alice/session_key
 base64 -w 0 alice/ciphertext > alice/ciphertext.base64
-cat alice/ciphertext.base64
+cat_safe alice/ciphertext.base64
 echo ""
 
-echo_wait $CYAN "--> Alice: Encrypt SESSION KEY with Bob's PUBLIC KEY"
+echo_alice "Alice: Encrypt SESSION KEY with Bob's PUBLIC KEY"
 openssl rsautl -encrypt -inkey alice/bob_pub_key.pem -pubin -in alice/session_key -out alice/session_key_ciphertext
 base64 -w 0 alice/session_key_ciphertext > alice/session_key_ciphertext.base64
-cat alice/session_key_ciphertext.base64
+cat_safe alice/session_key_ciphertext.base64
 echo ""
 
-echo_wait $CYAN "--> Alice: Compose message"
+echo_alice "Alice: Compose message"
 echo "MESSAGE:" > alice/message
 cat alice/ciphertext.base64 >> alice/message
 echo "" >> alice/message
 echo "SESSION_KEY:" >> alice/message
 cat alice/session_key_ciphertext.base64 >> alice/message
 echo "" >> alice/message
-cat alice/message
+cat_safe  alice/message
 
-echo_wait $CYAN "--> Alice send message"
+echo_alice "Alice send message"
 echo "cp alice/message bob/."
 cp alice/message bob/.
 
-echo_wait $YELLOW "--> Bob: Receive message"
-cat bob/message
+echo_bob "Bob: Receive message"
+cat_safe bob/message
 
 sed '2!d' alice/message > bob/ciphertext.base64
 sed '4!d' alice/message > bob/session_key_ciphertext.base64
 base64 --decode bob/ciphertext.base64 > bob/ciphertext
 base64 --decode bob/session_key_ciphertext.base64 > bob/session_key_ciphertext
 
-echo_wait $YELLOW "--> Bob: Decrypt SESSION KEY with PRIVATE KEY"
+echo_bob "Bob: Decrypt SESSION KEY with PRIVATE KEY"
 openssl rsautl -decrypt -inkey bob/bob_priv_enc_key.pem -in bob/session_key_ciphertext -out bob/session_key
-cat bob/session_key
+cat_unsafe bob/session_key
 
-echo_wait $YELLOW "--> Bob: Decrypt ciphertext with SESSION KEY"
+echo_bob "Bob: Decrypt ciphertext with SESSION KEY"
 openssl enc -des3 -d -in bob/ciphertext -out bob/plaintext -pass file:bob/session_key
-cat bob/plaintext
+cat_unsafe bob/plaintext
