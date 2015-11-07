48d84ea7e5621e8a2e281c4c571e417c84174011
James Booth <boothj5@gmail.com>
2015-11-07 19:19:00 +0000

    Simplified example 1

--- 1-simple.sh
+++ 1-simple.sh
@@ -1,50 +1,37 @@
 #!/bin/bash
 
-# Generates keypairs
-# Alice sends message to Bob
-
 . ./common.sh
 
 trap error_handler ERR
 trap exit_handler EXIT
 
-# key generation
-echo_wait $GREEN "--> Generate Alice's private key"
-openssl genrsa -out alice/alice_priv_key.pem 4096
-cat alice/alice_priv_key.pem
-
-echo_wait $GREEN "--> Extract Alice's public key"
-openssl rsa -pubout -in alice/alice_priv_key.pem -out alice/alice_pub_key.pem
-cat alice/alice_pub_key.pem
-
-echo_wait $GREEN "--> Generate Bob's private key"
+echo_wait $GREEN "--> Bob: Generate PRIVATE KEY"
 openssl genrsa -out bob/bob_priv_key.pem 4096
 cat bob/bob_priv_key.pem
 
-echo_wait $GREEN "--> Extract Bob's public key"
+echo_wait $GREEN "--> Bob: Extract PUBLIC KEY from PRIVATE_KEY"
 openssl rsa -pubout -in bob/bob_priv_key.pem -out bob/bob_pub_key.pem
 cat bob/bob_pub_key.pem
 
-# key exchange
-echo_wait $GREEN "--> Exchange public keys"
-cp alice/alice_pub_key.pem bob/.
+echo_wait $GREEN "--> Bob: Send PUBLIC KEY to Alice"
+echo "cp bob/bob_pub_key.pem alice/."
 cp bob/bob_pub_key.pem alice/.
 
-# Alice sends
-echo_wait $GREEN "--> Alice create message"
+echo_wait $GREEN "--> Alice: Create message"
 echo "Hello this is a private message from Alice to Bob..." > alice/plaintext
 cat alice/plaintext
 
-echo_wait $GREEN "--> Alice encrypt plaintext"
+echo_wait $GREEN "--> Alice: Encrypt plaintext with Bob's PUBLIC KEY"
 openssl rsautl -encrypt -inkey alice/bob_pub_key.pem -pubin -in alice/plaintext -out alice/ciphertext
-base64 alice/ciphertext > alice/ciphertext.base64
-cat alice/ciphertext.base64
+base64 alice/ciphertext > alice/message
+cat alice/message
 
-echo_wait $GREEN "--> Alice send message"
-cp alice/ciphertext.base64 bob/.
+echo_wait $GREEN "--> Alice: Send message to Bob"
+echo "cp alice/message bob/."
+cp alice/message bob/.
 
 # Bob receives
-echo_wait $GREEN "--> Bob decrypt ciphertext"
-base64 --decode bob/ciphertext.base64 > bob/ciphertext
+echo_wait $GREEN "--> Bob: Decrypt message with Bob's PRIVATE_KEY"
+base64 --decode bob/message > bob/ciphertext
 openssl rsautl -decrypt -inkey bob/bob_priv_key.pem -in bob/ciphertext -out bob/plaintext
 cat bob/plaintext
