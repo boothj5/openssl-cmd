785f2ff4e1515c32d86f35a5a51e6c1af6d5a1f6
James Booth <boothj5@gmail.com>
2015-11-07 21:31:30 +0000

    Use colour functions for example 2

--- 2-encrypt-priv.sh
+++ 2-encrypt-priv.sh
@@ -8,37 +8,37 @@ PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 trap error_handler ERR
 trap exit_handler EXIT
 
-echo_wait $YELLOW "--> Bob: Generate PRIVATE KEY"
+echo_bob "Bob: Generate PRIVATE KEY"
 openssl genrsa -out bob/bob_priv_key.pem 4096
-cat bob/bob_priv_key.pem
+cat_unsafe bob/bob_priv_key.pem
 
-echo_wait $YELLOW "--> Bob: Encrypt PRIVATE_KEY"
+echo_bob "Bob: Encrypt PRIVATE_KEY"
 openssl rsa -in bob/bob_priv_key.pem -des3 -out bob/bob_priv_enc_key.pem
-cat bob/bob_priv_enc_key.pem
+cat_safe bob/bob_priv_enc_key.pem
 
-echo_wait $YELLOW "--> Bob: Extract PUBLIC KEY from PRIVATE KEY"
+echo_bob "Bob: Extract PUBLIC KEY from PRIVATE KEY"
 openssl rsa -pubout -in bob/bob_priv_enc_key.pem -out bob/bob_pub_key.pem
-cat bob/bob_pub_key.pem
+cat_safe bob/bob_pub_key.pem
 
-echo_wait $YELLOW "--> Bob: Send PUBLIC KEY to Alice"
+echo_bob "Bob: Send PUBLIC KEY to Alice"
 echo "cp bob/bob_pub_key.pem alice/."
 cp bob/bob_pub_key.pem alice/.
 
-echo_wait $CYAN "--> Alice: Create message"
+echo_alice "Alice: Create message"
 echo "Hello this is a private message from Alice to Bob..." > alice/plaintext
-cat alice/plaintext
+cat_unsafe alice/plaintext
 
-echo_wait $CYAN "--> Alice: Encrypt plaintext with Bob's PUBLIC KEY"
+echo_alice "Alice: Encrypt plaintext with Bob's PUBLIC KEY"
 openssl rsautl -encrypt -inkey alice/bob_pub_key.pem -pubin -in alice/plaintext -out alice/ciphertext
 base64 -w 0 alice/ciphertext > alice/message
-cat alice/message
+cat_safe alice/message
 echo ""
 
-echo_wait $CYAN "--> Alice: Send message to Bob"
+echo_alice "Alice: Send message to Bob"
 echo "cp alice/message bob/."
 cp alice/message bob/.
 
-echo_wait $YELLOW "--> Bob: Decrypt message with Bob's PRIVATE KEY"
+echo_bob "--> Bob: Decrypt message with Bob's PRIVATE KEY"
 base64 --decode bob/message > bob/ciphertext
 openssl rsautl -decrypt -inkey bob/bob_priv_enc_key.pem -in bob/ciphertext -out bob/plaintext
-cat bob/plaintext
+cat_unsafe bob/plaintext
